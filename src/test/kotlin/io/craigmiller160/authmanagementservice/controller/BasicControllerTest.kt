dd0351d7f9b40b99defe5aafc672fbeef8a5e25d
Craig Miller <craigmiller160@gmail.com>
2020-06-25 13:28:12 -0400

    Stubbed out tests

---
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/controller/BasicControllerTest.kt
@@ -0,0 +1,19 @@
+package io.craigmiller160.authmanagementservice.controller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+
+@WebMvcTest
+class BasicControllerTest {
+
+    @Test
+    fun test_getUsers() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_getClients() {
+        TODO("Finish this")
+    }
+
+}
---
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/service/BasicServiceTest.kt
@@ -0,0 +1,20 @@
+package io.craigmiller160.authmanagementservice.service
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class BasicServiceTest {
+
+    @Test
+    fun test_getUsers() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_getClients() {
+        TODO("Finish this")
+    }
+
+}
