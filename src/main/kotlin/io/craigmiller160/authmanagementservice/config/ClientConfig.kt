ce52e4818254cdfceb97a6265665bff1fa3e811d
Craig Miller <craigmiller160@gmail.com>
2020-06-25 14:19:41 -0400

    Added ClientConfig

---
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/config/ClientConfig.kt
@@ -0,0 +1,22 @@
+package io.craigmiller160.authmanagementservice.config
+
+import io.craigmiller160.authmanagementservice.client.AuthServerClient
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class ClientConfig {
+
+    @Bean
+    fun restTemplate(): RestTemplate {
+        return RestTemplateBuilder().build()
+    }
+
+    @Bean
+    fun authServerClient(restTemplate: RestTemplate): AuthServerClient {
+        return AuthServerClient(restTemplate)
+    }
+
+}
