cc99b9b38fd9c9488d7af463997c516cd8cf6fa8
Craig Miller <craigmiller160@gmail.com>
2020-06-25 17:32:19 -0400

    Made sure the JWK settings are properly cached

--- src/main/kotlin/io/craigmiller160/authmanagementservice/security/JwtFilterConfigurer.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/security/JwtFilterConfigurer.kt
@@ -1,19 +1,29 @@
 package io.craigmiller160.authmanagementservice.security
 
+import com.nimbusds.jose.jwk.JWKSet
 import io.craigmiller160.authmanagementservice.config.AuthServerConfig
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.stereotype.Component
+import java.net.URL
+import javax.annotation.PostConstruct
 
 @Component
 class JwtFilterConfigurer (
         private val authServerConfig: AuthServerConfig
 ) : SecurityConfigurerAdapter<DefaultSecurityFilterChain,HttpSecurity>() {
 
+    private lateinit var jwkSet: JWKSet
+
+    @PostConstruct
+    fun loadJWKSet() {
+        jwkSet = JWKSet.load(URL("${authServerConfig.host}${authServerConfig.jwkPath}"))
+    }
+
     override fun configure(http: HttpSecurity?) {
-        http?.addFilterBefore(JwtValidationFilter(authServerConfig), UsernamePasswordAuthenticationFilter::class.java)
+        http?.addFilterBefore(JwtValidationFilter(authServerConfig, jwkSet), UsernamePasswordAuthenticationFilter::class.java)
     }
 
 }
--- src/main/kotlin/io/craigmiller160/authmanagementservice/security/JwtValidationFilter.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/security/JwtValidationFilter.kt
@@ -2,6 +2,8 @@ package io.craigmiller160.authmanagementservice.security
 
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.crypto.RSASSAVerifier
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
 import com.nimbusds.jose.jwk.source.RemoteJWKSet
 import com.nimbusds.jose.proc.JWSVerificationKeySelector
 import com.nimbusds.jose.proc.SecurityContext
@@ -11,12 +13,14 @@ import io.craigmiller160.authmanagementservice.config.AuthServerConfig
 import org.springframework.web.filter.OncePerRequestFilter
 import java.net.URL
 import java.security.interfaces.RSAPublicKey
+import javax.annotation.PostConstruct
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 class JwtValidationFilter (
-        private val authServerConfig: AuthServerConfig
+        private val authServerConfig: AuthServerConfig,
+        private val jwkSet: JWKSet
 ) : OncePerRequestFilter() {
 
     // TODO make this more configurable to switch between header and cookie
@@ -25,7 +29,7 @@ class JwtValidationFilter (
         val token = getToken(req)
 
         val jwtProcessor = DefaultJWTProcessor<SecurityContext>()
-        val keySource = RemoteJWKSet<SecurityContext>(URL("${authServerConfig.host}${authServerConfig.jwkPath}"))
+        val keySource = ImmutableJWKSet<SecurityContext>(jwkSet)
         val expectedAlg = JWSAlgorithm.RS256
         val keySelector = JWSVerificationKeySelector(expectedAlg, keySource)
         jwtProcessor.jwsKeySelector = keySelector
@@ -33,8 +37,6 @@ class JwtValidationFilter (
         val claims = jwtProcessor.process(token, null)
         println("CLAIMS") // TODO delete this
         println(claims) // TODO delete this
-
-        // TODO not sure if expired tokens are handled here
 
         chain.doFilter(req, res)
     }
