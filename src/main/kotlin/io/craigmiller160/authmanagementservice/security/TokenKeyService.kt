a3c2ee070132f281b4813941cac583b6a93af5b0
Craig Miller <craigmiller160@gmail.com>
2020-06-25 15:02:19 -0400

    Some tweaks

--- src/main/kotlin/io/craigmiller160/authmanagementservice/security/TokenKeyService.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/security/TokenKeyService.kt
@@ -1,18 +1,20 @@
 package io.craigmiller160.authmanagementservice.security
 
+import com.nimbusds.jose.jwk.JWKSet
 import io.craigmiller160.authmanagementservice.client.AuthServerClient
 import org.springframework.stereotype.Service
+import java.net.URL
 import javax.annotation.PostConstruct
 
 @Service
 class TokenKeyService (
-        private val authServerClient: AuthServerClient
+        private val authServerClient: AuthServerClient // TODO remove this
 ) {
 
     @PostConstruct
     fun loadKey() {
-        val jwk = authServerClient.getJwk()
-        val key = jwk.n
+        // TODO add error handling here... and make it configurable
+        val jwkSet = JWKSet.load(URL("https://localhost:7003/jwk"))
     }
 
 }
