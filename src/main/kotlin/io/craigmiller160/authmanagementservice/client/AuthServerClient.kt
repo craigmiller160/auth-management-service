6c10fe29b8ecb3d50bb37f35f90e88c6891a060d
Craig Miller <craigmiller160@gmail.com>
2020-06-25 14:48:18 -0400

    Cleaned up AuthServerClient

--- src/main/kotlin/io/craigmiller160/authmanagementservice/client/AuthServerClient.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/client/AuthServerClient.kt
@@ -1,5 +1,6 @@
 package io.craigmiller160.authmanagementservice.client
 
+import io.craigmiller160.authmanagementservice.dto.Jwk
 import io.craigmiller160.authmanagementservice.dto.JwkList
 import org.springframework.web.client.RestTemplate
 
@@ -9,10 +10,10 @@ class AuthServerClient (
 
     // TODO make this configurable
 
-    fun getJwk() {
-        val jwkList = restTemplate.getForEntity("https://localhost:7003/jwk", JwkList::class.java)
-        println("JWK List") // TODO delete this
-        println(jwkList) // TODO delete this
+    fun getJwk(): Jwk {
+        val response = restTemplate.getForEntity("https://localhost:7003/jwk", JwkList::class.java)
+        val jwkList: JwkList = response.body ?: throw Exception("")
+        return jwkList.keys[0]
     }
 
 }
