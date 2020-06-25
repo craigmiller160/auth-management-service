843588d2114db5130cad6232c5eda74f687f6222
Craig Miller <craigmiller160@gmail.com>
2020-06-25 17:36:49 -0400

    Fully implemented authentication

--- src/main/kotlin/io/craigmiller160/authmanagementservice/security/JwtValidationFilter.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/security/JwtValidationFilter.kt
@@ -5,7 +5,14 @@ import com.nimbusds.jose.jwk.JWKSet
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet
 import com.nimbusds.jose.proc.JWSVerificationKeySelector
 import com.nimbusds.jose.proc.SecurityContext
+import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.jwt.proc.DefaultJWTProcessor
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
@@ -16,21 +23,33 @@ class JwtValidationFilter (
 ) : OncePerRequestFilter() {
 
     // TODO make this more configurable to switch between header and cookie
+    // TODO need to validate that the token is for this app
 
     override fun doFilterInternal(req: HttpServletRequest, res: HttpServletResponse, chain: FilterChain) {
         val token = getToken(req)
+        val claims = validateToken(token)
+        SecurityContextHolder.getContext().authentication = createAuthentication(claims)
+        chain.doFilter(req, res)
+    }
 
+    private fun validateToken(token: String): JWTClaimsSet {
         val jwtProcessor = DefaultJWTProcessor<SecurityContext>()
         val keySource = ImmutableJWKSet<SecurityContext>(jwkSet)
         val expectedAlg = JWSAlgorithm.RS256
         val keySelector = JWSVerificationKeySelector(expectedAlg, keySource)
         jwtProcessor.jwsKeySelector = keySelector
 
-        val claims = jwtProcessor.process(token, null)
-        println("CLAIMS") // TODO delete this
-        println(claims) // TODO delete this
+        return jwtProcessor.process(token, null)
+    }
 
-        chain.doFilter(req, res)
+    private fun createAuthentication(claims: JWTClaimsSet): Authentication {
+        val authorities = claims.getStringListClaim("roles")
+                .map { SimpleGrantedAuthority(it) }
+        val userDetails = User.withUsername(claims.subject)
+                .password("")
+                .authorities(authorities)
+                .build()
+        return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
     }
 
     private fun getToken(req: HttpServletRequest): String {
