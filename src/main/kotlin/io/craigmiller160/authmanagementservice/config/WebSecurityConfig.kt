7132fdfff03510fb1eeeb63d18357bb55f26eb75
Craig Miller <craigmiller160@gmail.com>
2020-06-25 16:53:06 -0400

    Added authentication

--- src/main/kotlin/io/craigmiller160/authmanagementservice/config/WebSecurityConfig.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/config/WebSecurityConfig.kt
@@ -1,9 +1,18 @@
 package io.craigmiller160.authmanagementservice.config
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true
+)
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
@@ -12,6 +21,9 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     .and()
                     .requiresChannel().anyRequest().requiresSecure()
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest().fullyAuthenticated()
         }
     }
 
