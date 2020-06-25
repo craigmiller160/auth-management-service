c5e65409330d6fe618a6cc610a8c5767df0b8520
Craig Miller <craigmiller160@gmail.com>
2020-06-25 13:30:55 -0400

    Implemented BasicServiceTest

--- src/test/kotlin/io/craigmiller160/authmanagementservice/service/BasicServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/service/BasicServiceTest.kt
@@ -1,20 +1,67 @@
 package io.craigmiller160.authmanagementservice.service
 
+import io.craigmiller160.authmanagementservice.entity.Client
+import io.craigmiller160.authmanagementservice.entity.User
+import io.craigmiller160.authmanagementservice.repository.ClientRepository
+import io.craigmiller160.authmanagementservice.repository.UserRepository
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 
 @ExtendWith(MockitoExtension::class)
 class BasicServiceTest {
 
+    @Mock
+    private lateinit var userRepo: UserRepository
+    @Mock
+    private lateinit var clientRepo: ClientRepository
+
+    @InjectMocks
+    private lateinit var basicService: BasicService
+
+    private val user = User(
+            id = 0,
+            email = "craig@gmail.com",
+            firstName = "Craig",
+            lastName = "Miller",
+            password = "password"
+    )
+
+    private val client = Client(
+            id = 0,
+            name = "Client",
+            clientKey = "Key",
+            clientSecret = "Secret",
+            enabled = true,
+            allowClientCredentials = false,
+            allowPassword = false,
+            allowAuthCode = false,
+            accessTokenTimeoutSecs = 0,
+            refreshTokenTimeoutSecs = 0
+    )
+
     @Test
     fun test_getUsers() {
-        TODO("Finish this")
+        val users = listOf(user)
+        `when`(userRepo.findAll())
+                .thenReturn(users)
+
+        val result = basicService.getUsers()
+        assertEquals(users, result)
     }
 
     @Test
     fun test_getClients() {
-        TODO("Finish this")
+        val clients = listOf(client)
+        `when`(clientRepo.findAll())
+                .thenReturn(clients)
+
+        val result = basicService.getClients()
+        assertEquals(clients, result)
     }
 
 }
