b395dca6ab345b20bced788e1a503f3edd7d9e6c
Craig Miller <craigmiller160@gmail.com>
2020-06-26 19:16:47 -0400

    Stubbed out tests

--- src/test/kotlin/io/craigmiller160/authmanagementservice/security/AuthEntryPointTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/security/AuthEntryPointTest.kt
@@ -1,11 +1,28 @@
 package io.craigmiller160.authmanagementservice.security
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
 
+@ExtendWith(MockitoExtension::class)
 class AuthEntryPointTest {
 
+    @Mock
+    private lateinit var objectMapper: ObjectMapper
+
+    @InjectMocks
+    private lateinit var authEntryPoint: AuthEntryPoint
+
     @Test
-    fun test() {
+    fun test_alreadyErrorStatus() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_setUnauthorized() {
         TODO("Finish this")
     }
 
