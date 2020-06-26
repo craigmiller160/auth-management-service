eaa7a0fc04ddf9a062544e598ec805b3705968f4
Craig Miller <craigmiller160@gmail.com>
2020-06-25 20:46:42 -0400

    Stubbed out test

---
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/security/AuthEntryPointTest.kt
@@ -0,0 +1,12 @@
+package io.craigmiller160.authmanagementservice.security
+
+import org.junit.jupiter.api.Test
+
+class AuthEntryPointTest {
+
+    @Test
+    fun test() {
+        TODO("Finish this")
+    }
+
+}
