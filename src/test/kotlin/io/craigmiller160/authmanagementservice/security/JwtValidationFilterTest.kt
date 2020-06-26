3d2a6603dfd527dc3c790e38f5285b297881b2a3
Craig Miller <craigmiller160@gmail.com>
2020-06-26 19:32:40 -0400

    Stubbed out more tests

--- src/test/kotlin/io/craigmiller160/authmanagementservice/security/JwtValidationFilterTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/security/JwtValidationFilterTest.kt
@@ -1,8 +1,41 @@
 package io.craigmiller160.authmanagementservice.security
 
+import com.nimbusds.jose.jwk.JWKSet
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.security.core.context.SecurityContextHolder
+
+@ExtendWith(MockitoExtension::class)
 class JwtValidationFilterTest {
 
-    fun test() {
+    @Mock
+    private lateinit var jwkSet: JWKSet // TODO probably can't mock this to test this class
+
+    @InjectMocks
+    private lateinit var jwtValidationFilter: JwtValidationFilter
+
+    @BeforeEach
+    fun setup() {
+
+    }
+
+    @AfterEach
+    fun clean() {
+        SecurityContextHolder.clearContext()
+    }
+
+    @Test
+    fun test_doFilterInternal() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_doFilterInternal_fail() {
         TODO("Finish this")
     }
 
