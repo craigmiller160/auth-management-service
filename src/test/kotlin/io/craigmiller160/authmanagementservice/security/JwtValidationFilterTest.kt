35e4002ddaca2a42b94725a7746ccd060f0b264e
Craig <craigmiller160@gmail.com>
2020-06-27 11:27:23 -0400

    Improved test setup

--- src/test/kotlin/io/craigmiller160/authmanagementservice/security/JwtValidationFilterTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/security/JwtValidationFilterTest.kt
@@ -1,27 +1,39 @@
 package io.craigmiller160.authmanagementservice.security
 
+import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.KeyUse
+import com.nimbusds.jose.jwk.RSAKey
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.security.core.context.SecurityContextHolder
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPublicKey
 
 @ExtendWith(MockitoExtension::class)
 class JwtValidationFilterTest {
 
-    @Mock
-    private lateinit var jwkSet: JWKSet // TODO probably can't mock this to test this class
-
-    @InjectMocks
+    private lateinit var jwkSet: JWKSet
     private lateinit var jwtValidationFilter: JwtValidationFilter
+    private lateinit var keyPair: KeyPair
 
     @BeforeEach
     fun setup() {
+        val keyPairGen = KeyPairGenerator.getInstance("RSA")
+        keyPair = keyPairGen.genKeyPair()
 
+
+        val builder = RSAKey.Builder(keyPair.public as RSAPublicKey)
+                .keyUse(KeyUse.SIGNATURE)
+                .algorithm(JWSAlgorithm.RS256)
+                .keyID("oauth-jwt")
+        jwkSet = JWKSet(builder.build())
+
+        jwtValidationFilter = JwtValidationFilter(jwkSet)
     }
 
     @AfterEach
@@ -30,7 +42,12 @@ class JwtValidationFilterTest {
     }
 
     @Test
-    fun test_doFilterInternal() {
+    fun test_doFilterInternal_authHeader() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_doFilterInternal_cookie() {
         TODO("Finish this")
     }
 
