d04ab6941f39283250739bf4c5c20e3373166956
Craig Miller <craigmiller160@gmail.com>
2020-06-27 12:21:49 -0400

    Code cleanup

--- src/test/kotlin/io/craigmiller160/authmanagementservice/controller/BasicControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/controller/BasicControllerTest.kt
@@ -10,14 +10,12 @@ import io.craigmiller160.authmanagementservice.security.JwtFilterConfigurer
 import io.craigmiller160.authmanagementservice.service.BasicService
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestPropertySource
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
