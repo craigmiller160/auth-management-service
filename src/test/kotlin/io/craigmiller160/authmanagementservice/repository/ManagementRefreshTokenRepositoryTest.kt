e41281085316f5369f1e173ffe9115865b650bf2
Craig Miller <craigmiller160@gmail.com>
2020-07-06 19:41:05 -0400

    Stubbed out ManagementRefreshTokenRepositoryTest

--- src/test/kotlin/io/craigmiller160/authmanagementservice/repository/ManagementRefreshTokenRepositoryTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/repository/ManagementRefreshTokenRepositoryTest.kt
@@ -1,11 +1,22 @@
 package io.craigmiller160.authmanagementservice.repository
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
+@DataJpaTest
 class ManagementRefreshTokenRepositoryTest {
 
+    @Autowired
+    private lateinit var manageRefreshTokenRepo: ManagementRefreshTokenRepository
+
     @Test
-    fun test() {
+    fun test_findByTokenId() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_removeByTokenId() {
         TODO("Finish this")
     }
 
