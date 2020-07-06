177859b3324ee76ed0bdb4b71c7bc8d0cd33e559
Craig Miller <craigmiller160@gmail.com>
2020-07-06 16:01:37 -0400

    Fixed bug in repo method.

--- src/main/kotlin/io/craigmiller160/authmanagementservice/repository/ManagementRefreshTokenRepository.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/repository/ManagementRefreshTokenRepository.kt
@@ -2,13 +2,17 @@ package io.craigmiller160.authmanagementservice.repository
 
 import io.craigmiller160.authmanagementservice.entity.ManagementRefreshToken
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.stereotype.Repository
+import javax.transaction.Transactional
 
 @Repository
 interface ManagementRefreshTokenRepository : JpaRepository<ManagementRefreshToken,Long> {
 
     fun findByTokenId(tokenId: String): ManagementRefreshToken? // TODO add to unit tests
 
+    @Transactional
+    @Modifying
     fun removeByTokenId(tokenId: String): Int // TODO add to unit tests
 
 }
