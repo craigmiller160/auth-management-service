669476e5ef5e62a8779b080b56d1f30c5532b800
Craig Miller <craigmiller160@gmail.com>
2020-07-06 19:23:23 -0400

    Added retry mechanism and stubbed out tests for OAuthConfigTest

--- src/main/kotlin/io/craigmiller160/authmanagementservice/config/OAuthConfig.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/config/OAuthConfig.kt
@@ -1,8 +1,11 @@
 package io.craigmiller160.authmanagementservice.config
 
 import com.nimbusds.jose.jwk.JWKSet
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import java.lang.RuntimeException
 import java.net.URL
 import javax.annotation.PostConstruct
 
@@ -24,11 +27,23 @@ data class OAuthConfig (
         var cookieMaxAgeSecs: Long = 0
 ) {
 
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
+
     lateinit var jwkSet: JWKSet
 
     @PostConstruct
     fun loadJWKSet() {
-        jwkSet = JWKSet.load(URL("$authServerHost$jwkPath"))
+        for (i in 0 until 5) {
+            try {
+                jwkSet = JWKSet.load(URL("$authServerHost$jwkPath"))
+                return
+            } catch (ex: Exception) {
+                log.error("Error loading JWKSet", ex)
+                Thread.sleep(1000)
+            }
+        }
+
+        throw RuntimeException("Failed to load JWKSet")
     }
 
 }
---
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/config/OAuthConfigTest.kt
@@ -0,0 +1,24 @@
+package io.craigmiller160.authmanagementservice.config
+
+import org.junit.jupiter.api.Test
+
+class OAuthConfigTest {
+
+    private val oAuthConfig = OAuthConfig()
+
+    @Test
+    fun test_loadJWKSet_firstTrySuccess() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_loadJWKSet_secondTrySuccess() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_loadJWKSet_failure() {
+        TODO("Finish this")
+    }
+
+}
