9c568303a3484557c3c16ca77454bbbb8ffa5559
Craig Miller <craigmiller160@gmail.com>
2020-07-07 21:26:33 -0400

    Fixed logging and made other changes to migrate all the client config to the oauth2 lib

--- src/main/kotlin/io/craigmiller160/authmanagementservice/config/ClientConfig.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/config/ClientConfig.kt
@@ -1,35 +1,16 @@
 package io.craigmiller160.authmanagementservice.config
 
-import io.craigmiller160.authmanagementservice.client.RequestResponseLoggingInterceptor
-import io.craigmiller160.oauth2.client.AuthServerClient
-import io.craigmiller160.oauth2.client.AuthServerClientImpl
-import io.craigmiller160.oauth2.config.OAuthConfig
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.client.BufferingClientHttpRequestFactory
-import org.springframework.http.client.SimpleClientHttpRequestFactory
-import org.springframework.http.converter.FormHttpMessageConverter
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 
 @Configuration
-class ClientConfig (
-        private val oAuthConfig: OAuthConfig
-) {
+class ClientConfig {
 
     @Bean
-    fun restTemplate(): RestTemplate {
-        return RestTemplateBuilder() // TODO move this to lib
-                .requestFactory { BufferingClientHttpRequestFactory(SimpleClientHttpRequestFactory()) }
-                .interceptors(listOf(RequestResponseLoggingInterceptor()))
-                .messageConverters(FormHttpMessageConverter(), MappingJackson2HttpMessageConverter())
-                .build()
-    }
-
-    @Bean
-    fun authServerClient(restTemplate: RestTemplate): AuthServerClient {
-        return AuthServerClientImpl(restTemplate, oAuthConfig)
+    fun restTemplate(authRestTemplateBuilder: RestTemplateBuilder): RestTemplate {
+        return authRestTemplateBuilder.build()
     }
 
 }
