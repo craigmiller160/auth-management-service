e260e255325618fd95f3923e6f66c96970d245cc
Craig Miller <craigmiller160@gmail.com>
2020-07-08 15:07:28 -0400

    Got token refresh working

--- src/main/kotlin/io/craigmiller160/authmanagementservice/config/OAuth2UtilsConfig.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/config/OAuth2UtilsConfig.kt
@@ -1,6 +1,5 @@
 package io.craigmiller160.authmanagementservice.config
 
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
