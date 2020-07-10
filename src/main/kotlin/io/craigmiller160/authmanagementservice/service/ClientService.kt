337b4aaaef39d8196a80d562df77cfe22058547f
Craig Miller <craigmiller160@gmail.com>
2020-07-10 17:18:20 -0400

    Fully implemented ClientService

--- src/main/kotlin/io/craigmiller160/authmanagementservice/service/ClientService.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/service/ClientService.kt
@@ -1,6 +1,8 @@
 package io.craigmiller160.authmanagementservice.service
 
 import io.craigmiller160.authmanagementservice.dto.ClientList
+import io.craigmiller160.authmanagementservice.entity.Client
+import io.craigmiller160.authmanagementservice.exception.EntityNotFoundException
 import io.craigmiller160.authmanagementservice.repository.ClientRepository
 import org.springframework.stereotype.Service
 
@@ -9,21 +11,36 @@ class ClientService (
         private val clientRepo: ClientRepository
 ) {
 
+    // TODO validate inputs
+
     fun getClients(): ClientList {
         val clients = clientRepo.findAll()
         return ClientList(clients)
     }
 
-    fun getClient(id: Long) {
-        TODO("Finish this")
+    fun getClient(id: Long): Client? {
+        return clientRepo.findById(id).orElse(null)
     }
 
-    fun saveClient() {
-        TODO("Finish this")
+    fun createClient(client: Client): Client {
+        return clientRepo.save(client)
     }
 
-    fun deleteClient() {
-        TODO("Finish this")
+    fun updateClient(id: Long, client: Client): Client {
+        val existing = clientRepo.findById(id)
+                .orElseThrow { EntityNotFoundException("Client not found for ID: $id") }
+        val finalClient = client.copy(
+                id = id,
+                clientSecret = if (client.clientSecret.isBlank()) existing.clientSecret else client.clientSecret
+        )
+        return clientRepo.save(client)
+    }
+
+    fun deleteClient(id: Long): Client {
+        val existing = clientRepo.findById(id)
+                .orElseThrow { EntityNotFoundException("Client not found for ID: $id") }
+        clientRepo.deleteById(id)
+        return existing
     }
 
 }
