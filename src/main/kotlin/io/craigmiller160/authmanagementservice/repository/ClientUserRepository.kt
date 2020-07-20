29daa9846e7d5c970bbd300ea88f64e17523d301
Craig Miller <craigmiller160@gmail.com>
2020-07-19 21:17:53 -0400

    Added new repositories

---
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/repository/ClientUserRepository.kt
@@ -0,0 +1,16 @@
+package io.craigmiller160.authmanagementservice.repository
+
+import io.craigmiller160.authmanagementservice.entity.ClientUser
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.stereotype.Repository
+import javax.transaction.Transactional
+
+@Repository
+interface ClientUserRepository : JpaRepository<ClientUser,Long> {
+
+    @Transactional
+    @Modifying
+    fun deleteAllByClientId(clientId: Long): Long
+
+}
