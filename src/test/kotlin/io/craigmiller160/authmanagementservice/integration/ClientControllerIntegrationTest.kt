ef5a8a784c1e0f91b2e67051bf8b675d83c98288
Craig Miller <craigmiller160@gmail.com>
2020-07-22 16:33:30 -0400

    Added token to integration test

--- src/test/kotlin/io/craigmiller160/authmanagementservice/integration/ClientControllerIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/ClientControllerIntegrationTest.kt
@@ -1,10 +1,15 @@
 package io.craigmiller160.authmanagementservice.integration
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.nimbusds.jose.jwk.JWKSet
 import io.craigmiller160.authmanagementservice.entity.Client
+import io.craigmiller160.authmanagementservice.testutils.JwtUtils
 import io.craigmiller160.oauth2.config.OAuthConfig
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -14,11 +19,25 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.security.KeyPair
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
 @AutoConfigureMockMvc
 class ClientControllerIntegrationTest {
+
+    companion object {
+
+        private lateinit var keyPair: KeyPair
+        private lateinit var jwkSet: JWKSet
+
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll() {
+            keyPair = JwtUtils.createKeyPair()
+            jwkSet = JwtUtils.createJwkSet(keyPair)
+        }
+    }
 
     @MockBean
     private lateinit var oauthConfig: OAuthConfig
@@ -28,6 +47,17 @@ class ClientControllerIntegrationTest {
 
     @Autowired
     private lateinit var objectMapper: ObjectMapper
+
+    private lateinit var token: String
+
+    @BeforeEach
+    fun setup() {
+        `when`(oauthConfig.jwkSet)
+                .thenReturn(jwkSet)
+
+        val jwt = JwtUtils.createJwt()
+        token = JwtUtils.signAndSerializeJwt(jwt, keyPair.private)
+    }
 
     @Test
     fun test_createClient() {
@@ -47,6 +77,7 @@ class ClientControllerIntegrationTest {
                 post("/clients")
                         .contentType("application/json")
                         .content(objectMapper.writeValueAsString(client))
+                        .header("Authorization", "Bearer $token")
                         .secure(true)
         )
                 .andDo(MockMvcResultHandlers.print())
