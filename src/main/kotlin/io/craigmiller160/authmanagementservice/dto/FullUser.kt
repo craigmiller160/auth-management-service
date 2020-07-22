30f1919fe0bb56bca0cf2cfd37ccb1832a1a10e3
Craig Miller <craigmiller160@gmail.com>
2020-07-22 15:27:39 -0400

    Another draft of the FullUserDto

--- src/main/kotlin/io/craigmiller160/authmanagementservice/dto/FullUser.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/dto/FullUser.kt
@@ -1,22 +1,16 @@
 package io.craigmiller160.authmanagementservice.dto
 
-import io.craigmiller160.authmanagementservice.entity.Client
-import io.craigmiller160.authmanagementservice.entity.Role
 import io.craigmiller160.authmanagementservice.entity.User
 
 data class FullUser (
         val user: User,
-        val client: Client,
-        val roles: List<Role>
+        val clients: List<FullUserClient>
 ) : Sanitizer<FullUser> {
-
-    // TODO each user has multiple clients, and can have one or more roles within that client
-    // TODO how does this work from a DTO and UI standpoint?
 
     override fun sanitize(): FullUser {
         val user = this.user.sanitize()
-        val client = this.client.sanitize()
-        return FullUser(user, client, this.roles)
+        val clients = this.clients.map { it.sanitize() }
+        return FullUser(user, clients)
     }
 
 }
