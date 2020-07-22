e643e2872850c7c0857046b6278b2bc77e0728c2
Craig Miller <craigmiller160@gmail.com>
2020-07-22 15:33:30 -0400

    Began preparing the more advanced logic for handling users

--- src/main/kotlin/io/craigmiller160/authmanagementservice/repository/ClientRepository.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/repository/ClientRepository.kt
@@ -2,11 +2,22 @@ package io.craigmiller160.authmanagementservice.repository
 
 import io.craigmiller160.authmanagementservice.entity.Client
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ClientRepository : JpaRepository<Client,Long> {
 
     fun findAllByOrderByName(): List<Client>
+
+    @Query("""
+        SELECT c 
+        FROM Client c 
+        JOIN ClientUser cu ON c.id = cu.clientId 
+        WHERE cu.userId = :userId
+        ORDER BY c.name ASC
+    """)
+    fun findAllByUserOrderByName(@Param("userId") userId: Long): List<Client>
 
 }
--- src/main/kotlin/io/craigmiller160/authmanagementservice/service/UserService.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/service/UserService.kt
@@ -3,6 +3,8 @@ package io.craigmiller160.authmanagementservice.service
 import io.craigmiller160.authmanagementservice.dto.UserList
 import io.craigmiller160.authmanagementservice.entity.User
 import io.craigmiller160.authmanagementservice.exception.EntityNotFoundException
+import io.craigmiller160.authmanagementservice.repository.ClientRepository
+import io.craigmiller160.authmanagementservice.repository.RoleRepository
 import io.craigmiller160.authmanagementservice.repository.UserRepository
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
@@ -10,7 +12,9 @@ import javax.transaction.Transactional
 
 @Service
 class UserService (
-        private val userRepo: UserRepository
+        private val userRepo: UserRepository,
+        private val clientRepo: ClientRepository,
+        private val roleRepo: RoleRepository
 ) {
 
     private val encoder = BCryptPasswordEncoder()
@@ -21,15 +25,18 @@ class UserService (
     }
 
     fun getUser(id: Long): User? {
+        // TODO need the rest of the data here
         return userRepo.findById(id).orElse(null)
     }
 
     fun createUser(user: User): User {
+        // TODO need to return an empty shell of the rest of the data here
         return userRepo.save(user)
     }
 
     @Transactional
     fun updateUser(id: Long, user: User): User {
+        // TODO need to save the rest of the data here
         val existing = userRepo.findById(id)
                 .orElseThrow { EntityNotFoundException("User not found for ID: $id") }
 
@@ -46,6 +53,8 @@ class UserService (
 
     @Transactional
     fun deleteUser(id: Long): User {
+        // TODO need to return the rest of the data here
+        // TODO also need to delete the other references here
         val existing = userRepo.findById(id)
                 .orElseThrow { EntityNotFoundException("User not found for ID: $id") }
         userRepo.deleteById(id)
