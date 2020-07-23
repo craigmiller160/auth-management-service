d8d53c4caa31eee91b423412eae65df0d97117fa
Craig Miller <craigmiller160@gmail.com>
2020-07-23 12:18:01 -0400

    Began stubbing out new tests

---
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/ClientRoleControllerIntegrationTest.kt
@@ -0,0 +1,15 @@
+package io.craigmiller160.authmanagementservice.integration
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@AutoConfigureMockMvc
+class ClientRoleControllerIntegrationTest {
+
+
+
+}
