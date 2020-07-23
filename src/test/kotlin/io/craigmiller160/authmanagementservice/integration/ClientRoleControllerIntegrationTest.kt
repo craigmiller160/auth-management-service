e4e312c6e40c2e51250b546c911694b2acb0c7e9
Craig Miller <craigmiller160@gmail.com>
2020-07-23 12:24:54 -0400

    Stubbed out all tests

--- src/test/kotlin/io/craigmiller160/authmanagementservice/integration/ClientRoleControllerIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/ClientRoleControllerIntegrationTest.kt
@@ -1,15 +1,67 @@
 package io.craigmiller160.authmanagementservice.integration
 
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
-@AutoConfigureMockMvc
-class ClientRoleControllerIntegrationTest {
+class ClientRoleControllerIntegrationTest : AbstractControllerIntegrationTest() {
 
+    @Test
+    fun test_getRoles() {
+        TODO("Finish this")
+    }
 
+    @Test
+    fun test_getRoles_noContent() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_getRoles_unauthorized() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_createRole() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_createRole_unauthorized() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_updateRole() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_updateRole_noMatch() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_updateRole_unauthorized() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_deleteRole() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_deleteRole_noMatch() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_deleteRole_unauthorized() {
+        TODO("Finish this")
+    }
 
 }
