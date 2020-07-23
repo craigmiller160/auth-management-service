952cbb4cc1c88c1588b3b8dbe26eff7f408c6923
Craig <craigmiller160@gmail.com>
2020-07-23 13:11:55 -0400

    Get all roles tests finished

--- src/test/kotlin/io/craigmiller160/authmanagementservice/integration/ClientRoleControllerIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/ClientRoleControllerIntegrationTest.kt
@@ -1,7 +1,17 @@
 package io.craigmiller160.authmanagementservice.integration
 
+import io.craigmiller160.authmanagementservice.dto.RoleList
+import io.craigmiller160.authmanagementservice.entity.Client
+import io.craigmiller160.authmanagementservice.entity.Role
+import io.craigmiller160.authmanagementservice.repository.ClientRepository
+import io.craigmiller160.authmanagementservice.repository.RoleRepository
+import io.craigmiller160.authmanagementservice.testutils.TestData
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
@@ -9,19 +19,69 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @ExtendWith(SpringExtension::class)
 class ClientRoleControllerIntegrationTest : AbstractControllerIntegrationTest() {
 
+    @Autowired
+    private lateinit var clientRepo: ClientRepository
+
+    @Autowired
+    private lateinit var roleRepo: RoleRepository
+
+    private lateinit var client1: Client
+    private lateinit var role1: Role
+    private lateinit var role2: Role
+
+    @BeforeEach
+    fun setup() {
+        client1 = clientRepo.save(TestData.createClient(1))
+        role1 = roleRepo.save(TestData.createRole(1, client1.id))
+        role2 = roleRepo.save(TestData.createRole(2, client1.id))
+    }
+
+    @AfterEach
+    fun clean() {
+        roleRepo.deleteAll()
+        clientRepo.deleteAll()
+    }
+
     @Test
     fun test_getRoles() {
-        TODO("Finish this")
+        val roleListResult = apiProcessor.call {
+            request {
+                path = "/clients/{clientId}/roles"
+                vars = arrayOf(client1.id)
+            }
+        }.convert(RoleList::class.java)
+
+        val roles = roleListResult.roles.sortedBy { it.name }
+        assertEquals(role1, roles[0])
+        assertEquals(role2, roles[1])
     }
 
     @Test
     fun test_getRoles_noContent() {
-        TODO("Finish this")
+        clean()
+        apiProcessor.call {
+            request {
+                path = "/clients/{clientId}/roles"
+                vars = arrayOf(client1.id)
+            }
+            response {
+                status = 204
+            }
+        }
     }
 
     @Test
     fun test_getRoles_unauthorized() {
-        TODO("Finish this")
+        apiProcessor.call {
+            request {
+                path = "/clients/{clientId}/roles"
+                vars = arrayOf(client1.id)
+                doAuth = false
+            }
+            response {
+                status = 401
+            }
+        }
     }
 
     @Test
