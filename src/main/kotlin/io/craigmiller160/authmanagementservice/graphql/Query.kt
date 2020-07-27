ff468a168d156d757333dc9ec8642ed88591082a
Craig Miller <craigmiller160@gmail.com>
2020-07-27 16:22:12 -0400

    Migrated to new version of library

--- src/main/kotlin/io/craigmiller160/authmanagementservice/graphql/Query.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/graphql/Query.kt
@@ -1,6 +1,6 @@
 package io.craigmiller160.authmanagementservice.graphql
 
-import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import graphql.kickstart.tools.GraphQLQueryResolver
 import io.craigmiller160.authmanagementservice.entity.Client
 import io.craigmiller160.authmanagementservice.repository.ClientRepository
 import org.springframework.stereotype.Component
