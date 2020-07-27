b1c2092c8aea4d5a07d1f085dca2bff6a5158ffd
Craig Miller <craigmiller160@gmail.com>
2020-07-27 16:48:40 -0400

    Added enhanced model mapper config

--- src/main/kotlin/io/craigmiller160/authmanagementservice/config/MapperConfig.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/config/MapperConfig.kt
@@ -1,4 +1,15 @@
 package io.craigmiller160.authmanagementservice.config
 
+import io.craigmiller160.modelmapper.EnhancedModelMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class MapperConfig {
+
+    @Bean
+    fun modelMapper(): EnhancedModelMapper {
+        return EnhancedModelMapper()
+    }
+
 }
