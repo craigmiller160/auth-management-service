7ec5bcac81c1566b2e8c60954821c58dd7c465c9
Craig Miller <craigmiller160@gmail.com>
2020-07-28 17:51:35 -0400

    Added client mutations to schema

---
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/graphql/mutation/ClientMutationResolver.kt
@@ -0,0 +1,11 @@
+package io.craigmiller160.authmanagementservice.graphql.mutation
+
+import graphql.kickstart.tools.GraphQLMutationResolver
+import org.springframework.stereotype.Component
+
+@Component
+class ClientMutationResolver : GraphQLMutationResolver {
+
+
+
+}
