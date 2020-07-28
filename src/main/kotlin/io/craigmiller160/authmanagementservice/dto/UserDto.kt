c757bdaff8bac8c4d742fa605e3705a072cd7730
Craig Miller <craigmiller160@gmail.com>
2020-07-28 15:59:28 -0400

    Removed password and clientSecret from user/client responses

--- src/main/kotlin/io/craigmiller160/authmanagementservice/dto/UserDto.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/dto/UserDto.kt
@@ -6,8 +6,7 @@ data class UserDto (
         val id: Long,
         val email: String,
         val firstName: String,
-        val lastName: String,
-        val password: String
+        val lastName: String
 ) {
     companion object {
         fun fromUser(user: User): UserDto {
@@ -15,8 +14,7 @@ data class UserDto (
                     id = user.id,
                     email = user.email,
                     firstName = user.firstName,
-                    lastName = user.lastName,
-                    password = user.password
+                    lastName = user.lastName
             )
         }
     }
