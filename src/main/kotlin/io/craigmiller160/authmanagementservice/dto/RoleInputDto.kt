c5e4291a98b61a5ffc2257386d25079a52fc3b24
Craig Miller <craigmiller160@gmail.com>
2020-07-28 16:10:30 -0400

    Got input type mutation working

---
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/dto/RoleInputDto.kt
@@ -0,0 +1,6 @@
+package io.craigmiller160.authmanagementservice.dto
+
+data class RoleInputDto (
+        val name: String,
+        val clientId: Long
+)
--- src/main/kotlin/io/craigmiller160/authmanagementservice/graphql/mutation/RoleMutationResolver.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/graphql/mutation/RoleMutationResolver.kt
@@ -2,6 +2,7 @@ package io.craigmiller160.authmanagementservice.graphql.mutation
 
 import graphql.kickstart.tools.GraphQLMutationResolver
 import io.craigmiller160.authmanagementservice.dto.RoleDto
+import io.craigmiller160.authmanagementservice.dto.RoleInputDto
 import io.craigmiller160.authmanagementservice.service.RoleService
 import org.springframework.stereotype.Component
 
@@ -12,6 +13,10 @@ class RoleMutationResolver (
 
     fun createRole(name: String, clientId: Long): RoleDto {
         return roleService.createRole(name, clientId)
+    }
+
+    fun createRole2(role: RoleInputDto): RoleDto {
+        return roleService.createRole(role.name, role.clientId) // TODO refactor if this works
     }
 
 }
