cba117771fe9fa53e98d0d2d6a55cc0434712991
Craig Miller <craigmiller160@gmail.com>
2020-07-28 16:58:30 -0400

    Beginning to work on createUser mutation

---
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/dto/UserInputDto.kt
@@ -0,0 +1,20 @@
+package io.craigmiller160.authmanagementservice.dto
+
+import io.craigmiller160.authmanagementservice.entity.User
+
+data class UserInputDto (
+        val email: String,
+        val password: String,
+        val firstName: String,
+        val lastName: String
+) {
+    fun toUser(): User {
+        return User(
+                id = 0,
+                email = this.email,
+                password = this.password,
+                firstName = this.firstName,
+                lastName = this.lastName
+        )
+    }
+}
---
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/graphql/mutation/UserMutationResolver.kt
@@ -0,0 +1,11 @@
+package io.craigmiller160.authmanagementservice.graphql.mutation
+
+import graphql.kickstart.tools.GraphQLMutationResolver
+
+class UserMutationResolver : GraphQLMutationResolver {
+
+    fun createUser() {
+
+    }
+
+}
