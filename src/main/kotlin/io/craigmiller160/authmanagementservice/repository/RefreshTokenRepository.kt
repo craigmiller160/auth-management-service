2ef035cde2fb6866ea3b137397d67d73afc5191f
Craig Miller <craigmiller160@gmail.com>
2020-08-02 11:26:33 -0400

    Added methods to RefreshTokenRepository

--- src/main/kotlin/io/craigmiller160/authmanagementservice/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/repository/RefreshTokenRepository.kt
@@ -5,4 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RefreshTokenRepository : JpaRepository<RefreshToken,String>
+interface RefreshTokenRepository : JpaRepository<RefreshToken,String> {
+
+    fun findByClientIdAndUserId(clientId: Long, userId: Long): RefreshToken?
+
+    fun findByClientIdAndUserIdIsNull(clientId: Long): RefreshToken?
+
+}
--- src/main/kotlin/io/craigmiller160/authmanagementservice/service/UserAuthService.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/service/UserAuthService.kt
@@ -1,10 +1,13 @@
 package io.craigmiller160.authmanagementservice.service
 
 import io.craigmiller160.authmanagementservice.dto.UserAuthDetailsDto
+import io.craigmiller160.authmanagementservice.repository.RefreshTokenRepository
 import org.springframework.stereotype.Service
 
 @Service
-class UserAuthService {
+class UserAuthService (
+        private val refreshTokenRepo: RefreshTokenRepository
+) {
 
     fun getUserAuthDetails(clientId: Long, userId: Long): UserAuthDetailsDto {
         TODO("Finish this")
