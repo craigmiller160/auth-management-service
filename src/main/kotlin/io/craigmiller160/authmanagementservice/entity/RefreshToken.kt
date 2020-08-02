07c83402ec8ab58dcbbeddee737e3ec71b11662a
Craig Miller <craigmiller160@gmail.com>
2020-08-02 11:22:58 -0400

    Added entity and repository

---
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/entity/RefreshToken.kt
@@ -0,0 +1,17 @@
+package io.craigmiller160.authmanagementservice.entity
+
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "refresh_tokens")
+data class RefreshToken (
+        @Id
+        val id: String,
+        val refreshToken: String,
+        val clientId: Long,
+        val userId: Long?,
+        val timestamp: LocalDateTime
+)
