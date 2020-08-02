a1c8f0416ba726111d97e20a5c598ce728ee8b7b
Craig Miller <craigmiller160@gmail.com>
2020-08-02 12:22:11 -0400

    Implemented ClientAuthService

--- src/main/kotlin/io/craigmiller160/authmanagementservice/service/ClientAuthService.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/service/ClientAuthService.kt
@@ -1,17 +1,38 @@
 package io.craigmiller160.authmanagementservice.service
 
 import io.craigmiller160.authmanagementservice.dto.ClientAuthDetailsDto
+import io.craigmiller160.authmanagementservice.repository.ClientRepository
+import io.craigmiller160.authmanagementservice.repository.RefreshTokenRepository
 import org.springframework.stereotype.Service
+import javax.persistence.EntityNotFoundException
 
 @Service
-class ClientAuthService {
+class ClientAuthService (
+        private val clientRepo: ClientRepository,
+        private val refreshTokenRepo: RefreshTokenRepository
+) {
 
     fun getClientAuthDetails(clientId: Long): ClientAuthDetailsDto {
-        TODO("Finish this")
+        clientRepo.findById(clientId)
+                .orElseThrow { EntityNotFoundException("No auth details found for client $clientId") }
+
+        val refreshToken = refreshTokenRepo.findByClientIdAndUserIdIsNull(clientId)
+        return ClientAuthDetailsDto(
+                tokenId = refreshToken?.id,
+                clientId = clientId,
+                lastAuthenticated = refreshToken?.timestamp
+        )
     }
 
     fun clearClientAuthDetails(clientId: Long): ClientAuthDetailsDto {
-        TODO("Finish this")
+        clientRepo.findById(clientId)
+                .orElseThrow { EntityNotFoundException("No auth details found for client $clientId") }
+        refreshTokenRepo.deleteByClientIdAndUserIdIsNull(clientId)
+        return ClientAuthDetailsDto(
+                tokenId = null,
+                clientId = clientId,
+                lastAuthenticated = null
+        )
     }
 
 }
