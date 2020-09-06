8b41871dacf536db293ad3755b2e30f347cca6c1
Craig Miller <craigmiller160@gmail.com>
2020-09-06 13:18:41 -0400

    Trying to get new tests to work

--- src/test/kotlin/io/craigmiller160/authmanagementservice/integration/ClientQueryIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/ClientQueryIntegrationTest.kt
@@ -56,13 +56,13 @@ class ClientQueryIntegrationTest : AbstractOAuthTest() {
                 ?: throw RuntimeException("Graphql file not found: $file")
     }
 
-//    @Test
-//    fun test() {
-//        val query = getGraphql("getAllClients") // TODO probably don't need this
-//        graphqlRestTemplate.addHeader("Authorization", "Bearer $token")
-//        val response = graphqlRestTemplate.postForResource("graphql/getAllClients.graphql")
-//        println(response.rawResponse.body) // TODO delete this
-//    }
+    @Test
+    fun test() {
+        val query = getGraphql("getAllClients") // TODO probably don't need this
+        graphqlRestTemplate.addHeader("Authorization", "Bearer $token")
+        val response = graphqlRestTemplate.postForResource("graphql/getAllClients.graphql")
+        println(response.rawResponse.body) // TODO delete this
+    }
 //
 //    @Test
 //    fun test2() {
@@ -71,6 +71,7 @@ class ClientQueryIntegrationTest : AbstractOAuthTest() {
 //        val json = """{"query": "$query", "variables" null, "operationName": null}"""
 //        val entity = RequestEntity.post(URI("/graphql"))
 //                .header("Authorization", "Bearer $token")
+//                .header("Content-Type", "application/json")
 //                .body(json)
 //
 //        val result = restTemplate.exchange(entity, String::class.java)
