64c27f8b938a2f0aad03708c1ac5264133dc7735
Craig Miller <craigmiller160@gmail.com>
2020-09-06 13:00:04 -0400

    Stubbed out some integration tests

---
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/service/ClientAuthServiceIntegrationTest.kt
@@ -0,0 +1,15 @@
+package io.craigmiller160.authmanagementservice.integration.service
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class ClientAuthServiceIntegrationTest {
+
+    @Test
+    fun test() {
+        TODO("Finish this")
+    }
+
+}
---
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/service/ClientServiceIntegrationTest.kt
@@ -0,0 +1,15 @@
+package io.craigmiller160.authmanagementservice.integration.service
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class ClientServiceIntegrationTest {
+
+    @Test
+    fun test() {
+        TODO("Finish this")
+    }
+
+}
