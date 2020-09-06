9ebcaf6c1fc3014b62273fcb3852a270bc67fb95
Craig Miller <craigmiller160@gmail.com>
2020-09-06 14:39:55 -0400

    Implemented more query tests

--- src/test/kotlin/io/craigmiller160/authmanagementservice/integration/graphql/query/ClientQueryIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/graphql/query/ClientQueryIntegrationTest.kt
@@ -15,10 +15,12 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ClientQueryIntegrationTest : AbstractOAuthTest() {
 
     @Autowired
@@ -32,13 +34,18 @@ class ClientQueryIntegrationTest : AbstractOAuthTest() {
 
     private lateinit var client1: Client
     private lateinit var client2: Client
+    private lateinit var client1Dto: ClientDto
+    private lateinit var client2Dto: ClientDto
 
     @BeforeEach
     fun setup() {
         client1 = clientRepo.save(TestData.createClient(1))
         client2 = clientRepo.save(TestData.createClient(2))
 
-        graphqlRestTemplate.addHeader("Authorization", "Bearer $token") // TODO adding it each time???
+        client1Dto = ClientDto.fromClient(client1)
+        client2Dto = ClientDto.fromClient(client2)
+
+        graphqlRestTemplate.addHeader("Authorization", "Bearer $token")
     }
 
     @AfterEach
@@ -49,10 +56,7 @@ class ClientQueryIntegrationTest : AbstractOAuthTest() {
     @Test
     fun `query - clients - base client only`() {
         val response = graphqlRestTemplate.postForResource("graphql/query_clients_baseClientOnly.graphql")
-        val body = response.rawResponse.body
-        val result = objectMapper.readValue(body, object: TypeReference<Response<ClientsResponse>>(){})
-        val client1Dto = ClientDto.fromClient(client1)
-        val client2Dto = ClientDto.fromClient(client2)
+        val result = objectMapper.readValue(response.rawResponse.body, object: TypeReference<Response<ClientsResponse>>(){})
 
         assertEquals(client1Dto, result.data.clients[0])
         assertEquals(client2Dto, result.data.clients[1])
@@ -70,11 +74,18 @@ class ClientQueryIntegrationTest : AbstractOAuthTest() {
 
     @Test
     fun `query - single client - base client only`() {
-        TODO("Finish this")
+        val response = graphqlRestTemplate.postForResource("graphql/query_singleClient_baseClientOnly.graphql")
+        val result = objectMapper.readValue(response.rawResponse.body, object: TypeReference<Response<ClientResponse>>(){})
+
+        assertEquals(client1Dto, result.data.client)
     }
 
     class ClientsResponse (
             val clients: List<ClientDto>
+    )
+
+    class ClientResponse (
+            val client: ClientDto
     )
 
     class Response<T> (
