3a201f2a9f1ca4ed3fc4cc5ad606bceded8fb536
Craig Miller <craigmiller160@gmail.com>
2020-09-06 14:42:21 -0400

    Stubbed out remaining integration test files

---
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/graphql/query/UserQueryIntegrationTest.kt
@@ -0,0 +1,15 @@
+package io.craigmiller160.authmanagementservice.integration.graphql.query
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class UserQueryIntegrationTest {
+
+    @Test
+    fun test() {
+        TODO("Finish this")
+    }
+
+}
