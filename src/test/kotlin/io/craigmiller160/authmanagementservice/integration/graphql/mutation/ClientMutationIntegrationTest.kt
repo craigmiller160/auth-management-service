3d32864a30940f914d907c62231287679ef3a7d4
Craig Miller <craigmiller160@gmail.com>
2020-09-06 17:02:51 -0400

    Wrote first ClientMutationIntegrationTest test

--- src/test/kotlin/io/craigmiller160/authmanagementservice/integration/graphql/mutation/ClientMutationIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/graphql/mutation/ClientMutationIntegrationTest.kt
@@ -1,22 +1,77 @@
 package io.craigmiller160.authmanagementservice.integration.graphql.mutation
 
+import io.craigmiller160.authmanagementservice.dto.ClientDto
+import io.craigmiller160.authmanagementservice.entity.Client
 import io.craigmiller160.authmanagementservice.integration.graphql.AbstractGraphqlTest
+import io.craigmiller160.authmanagementservice.repository.ClientRepository
+import io.craigmiller160.authmanagementservice.testutils.TestData
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ClientMutationIntegrationTest : AbstractGraphqlTest() {
 
+    @Autowired
+    private lateinit var clientRepo: ClientRepository
+
+    private val passwordEncoder = BCryptPasswordEncoder()
+
+    private lateinit var client1: Client
+
     override fun getGraphqlBasePath(): String {
         return "graphql/mutation/client"
     }
 
+    @BeforeEach
+    fun setup() {
+        client1 = clientRepo.save(TestData.createClient(1))
+    }
+
+    @AfterEach
+    fun clean() {
+        clientRepo.deleteAll()
+    }
+
+    private fun validateSecret(expected: String, actualHash: String) {
+        val secretHash = actualHash.replace("{bcrypt}", "")
+        assertTrue(passwordEncoder.matches(expected, secretHash))
+    }
+
     @Test
     fun `mutation - client - createClient`() {
-        TODO("Finish this")
+        val result = execute("mutation_client_createClient", CreateClientResponse::class.java)
+
+        val expected = ClientDto(
+                id = 2,
+                name = "New Client",
+                clientKey = "NewKey",
+                enabled = true,
+                accessTokenTimeoutSecs = 100,
+                refreshTokenTimeoutSecs = 200
+        )
+        assertEquals(expected, result.createClient)
+
+        val expectedDb = Client(
+                id = 2,
+                name = "New Client",
+                clientKey = "NewKey",
+                clientSecret = "",
+                enabled = true,
+                accessTokenTimeoutSecs = 100,
+                refreshTokenTimeoutSecs = 200
+        )
+        val actualDb = clientRepo.findById(2).get()
+        assertEquals(expectedDb, actualDb.copy(clientSecret = ""))
+        validateSecret("NewSecret", actualDb.clientSecret)
     }
 
     @Test
@@ -45,5 +100,17 @@ class ClientMutationIntegrationTest : AbstractGraphqlTest() {
     fun `mutation - client - addUserToClient`() {
         TODO("Finish this")
     }
+
+    class CreateClientResponse (
+            val createClient: ClientDto
+    )
+
+    class UpdateClientResponse (
+            val updateClient: ClientDto
+    )
+
+    class RemoveClientResponse (
+            val removeClient: ClientDto
+    )
 
 }
