57c4fbc521e07d13b760d1fa3a14e72bae1b464d
Craig Miller <craigmiller160@gmail.com>
2020-12-12 16:19:52 -0500

    Starting to refactor UserControllerIntegrationTest

--- src/test/kotlin/io/craigmiller160/authmanagementservice/integration/controller/UserControllerIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authmanagementservice/integration/controller/UserControllerIntegrationTest.kt
@@ -1,7 +1,6 @@
 package io.craigmiller160.authmanagementservice.integration.controller
 
 import io.craigmiller160.apitestprocessor.config.AuthType
-import io.craigmiller160.authmanagementservice.dto.UserAuthDetailsDto
 import io.craigmiller160.authmanagementservice.dto.UserAuthDetailsListDto
 import io.craigmiller160.authmanagementservice.entity.Client
 import io.craigmiller160.authmanagementservice.entity.ClientUser
@@ -19,11 +18,8 @@ import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.hasProperty
 import org.hamcrest.Matchers.hasSize
 import org.hamcrest.Matchers.notNullValue
-import org.hamcrest.Matchers.nullValue
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -31,7 +27,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpMethod
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import java.time.ZoneId
 import java.time.ZonedDateTime
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -50,31 +45,33 @@ class UserControllerIntegrationTest : AbstractControllerIntegrationTest() {
     @Autowired
     private lateinit var clientUserRepo: ClientUserRepository
 
-    private lateinit var userRefreshToken1: RefreshToken
-    private lateinit var userRefreshToken2: RefreshToken
-    private lateinit var clientRefreshToken: RefreshToken
+    private lateinit var client1user1Expired: RefreshToken
+    private lateinit var client1user1Valid: RefreshToken
+    private lateinit var client2user1Valid: RefreshToken
+    private lateinit var client1user2Valid: RefreshToken
+    private lateinit var client1userNullValid: RefreshToken
     private lateinit var client1: Client
     private lateinit var client2: Client
-    private lateinit var user: User
+    private lateinit var user1: User
+    private lateinit var user2: User
 
-    private val userToken1Id = "ABC"
-    private val userToken2Id = "MNO"
-    private val clientTokenId = "DEF"
-    private val userToken1 = "GHI"
-    private val userToken2 = "PQR"
-    private val clientToken = "JKL"
+    private val EXPIRED_TIMESTAMP: ZonedDateTime = ZonedDateTime.now().minusHours(1)
+    private val TIMESTAMP: ZonedDateTime = ZonedDateTime.now()
 
     @BeforeEach
     fun setup() {
         client1 = clientRepo.save(TestData.createClient(1))
         client2 = clientRepo.save(TestData.createClient(2))
-        user = userRepo.save(TestData.createUser(1))
-        clientUserRepo.save(ClientUser(0, user.id, client1.id))
-        clientUserRepo.save(ClientUser(0, user.id, client2.id))
+        user1 = userRepo.save(TestData.createUser(1))
+        user2 = userRepo.save(TestData.createUser(2))
+        clientUserRepo.save(ClientUser(0, user1.id, client1.id))
+        clientUserRepo.save(ClientUser(0, user1.id, client2.id))
 
-        userRefreshToken1 = refreshTokenRepo.save(RefreshToken(userToken1Id, userToken1, client1.id, user.id, ZonedDateTime.now(ZoneId.of("UTC"))))
-        userRefreshToken2 = refreshTokenRepo.save(RefreshToken(userToken2Id, userToken2, client2.id, user.id, ZonedDateTime.now(ZoneId.of("UTC"))))
-        clientRefreshToken = refreshTokenRepo.save(RefreshToken(clientTokenId, clientToken, client1.id, null, ZonedDateTime.now(ZoneId.of("UTC"))))
+        client1user1Expired = refreshTokenRepo.save(RefreshToken("Id1", "Token1", client1.id, user1.id, EXPIRED_TIMESTAMP))
+        client1user1Valid = refreshTokenRepo.save(RefreshToken("Id2", "Token2", client1.id, user1.id, TIMESTAMP))
+        client2user1Valid = refreshTokenRepo.save(RefreshToken("Id3", "Token3", client2.id, user1.id, TIMESTAMP))
+        client1user2Valid = refreshTokenRepo.save(RefreshToken("Id4", "Token4", client1.id, user2.id, TIMESTAMP))
+        client1userNullValid = refreshTokenRepo.save(RefreshToken("Id5", "Token5", client1.id, null, TIMESTAMP))
     }
 
     @AfterEach
@@ -87,21 +84,22 @@ class UserControllerIntegrationTest : AbstractControllerIntegrationTest() {
 
     @Test
     fun test_revokeUserAuthAccess() {
-        apiProcessor.call {
-            request {
-                method = HttpMethod.POST
-                path = "/users/auth/${user.id}/${client1.id}/revoke"
-            }
-            response {
-                status = 204
-            }
-        }
-
-        val tokens = refreshTokenRepo.findAll()
-        val sortedTokens = tokens.sortedBy { it.id }
-        assertEquals(2, sortedTokens.size)
-        assertEquals(clientRefreshToken.id, sortedTokens[0].id)
-        assertEquals(userRefreshToken2.id, sortedTokens[1].id)
+//        apiProcessor.call {
+//            request {
+//                method = HttpMethod.POST
+//                path = "/users/auth/${user1.id}/${client1.id}/revoke"
+//            }
+//            response {
+//                status = 204
+//            }
+//        }
+//
+//        val tokens = refreshTokenRepo.findAll()
+//        val sortedTokens = tokens.sortedBy { it.id }
+//        assertEquals(2, sortedTokens.size)
+//        assertEquals(clientRefreshToken.id, sortedTokens[0].id)
+//        assertEquals(userRefreshToken2.id, sortedTokens[1].id)
+        TODO("Finish this")
     }
 
     @Test
@@ -109,7 +107,7 @@ class UserControllerIntegrationTest : AbstractControllerIntegrationTest() {
         apiProcessor.call {
             request {
                 method = HttpMethod.POST
-                path = "/users/auth/${user.id}/1000/revoke"
+                path = "/users/auth/${user1.id}/1000/revoke"
             }
             response {
                 status = 400
@@ -122,7 +120,7 @@ class UserControllerIntegrationTest : AbstractControllerIntegrationTest() {
         apiProcessor.call {
             request {
                 method = HttpMethod.POST
-                path = "/users/auth/${user.id}/${client1.id}/revoke"
+                path = "/users/auth/${user1.id}/${client1.id}/revoke"
                 overrideAuth {
                     type = AuthType.NONE
                 }
@@ -137,7 +135,7 @@ class UserControllerIntegrationTest : AbstractControllerIntegrationTest() {
     fun test_getAllUserAuthDetails() {
         val result = apiProcessor.call {
             request {
-                path = "/users/auth/${user.id}"
+                path = "/users/auth/${user1.id}"
             }
         }.convert(UserAuthDetailsListDto::class.java)
 
@@ -148,17 +146,17 @@ class UserControllerIntegrationTest : AbstractControllerIntegrationTest() {
                 containsInAnyOrder(
                         allOf(
                                 hasProperty("clientId", equalTo(client1.id)),
-                                hasProperty("userId", equalTo(user.id)),
+                                hasProperty("userId", equalTo(user1.id)),
                                 hasProperty("lastAuthenticated", notNullValue()),
                                 hasProperty("clientName", equalTo(client1.name)),
-                                hasProperty("userEmail", equalTo(user.email))
+                                hasProperty("userEmail", equalTo(user1.email))
                         ),
                         allOf(
                                 hasProperty("clientId", equalTo(client2.id)),
-                                hasProperty("userId", equalTo(user.id)),
+                                hasProperty("userId", equalTo(user1.id)),
                                 hasProperty("lastAuthenticated", notNullValue()),
                                 hasProperty("clientName", equalTo(client2.name)),
-                                hasProperty("userEmail", equalTo(user.email))
+                                hasProperty("userEmail", equalTo(user1.email))
                         )
                 )
         ))
@@ -180,7 +178,7 @@ class UserControllerIntegrationTest : AbstractControllerIntegrationTest() {
     fun test_getAllUserAuthDetails_unauthorized() {
         apiProcessor.call {
             request {
-                path = "/users/auth/${user.id}"
+                path = "/users/auth/${user1.id}"
                 overrideAuth {
                     type = AuthType.NONE
                 }
