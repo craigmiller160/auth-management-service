242e1ae84c8bdaf4dde5fbcbd54062055133c799
Craig Miller <craigmiller160@gmail.com>
2020-12-27 14:49:19 -0500

    Set session creation policy to always

--- src/main/kotlin/io/craigmiller160/authmanagementservice/config/WebSecurityConfig.kt
+++ src/main/kotlin/io/craigmiller160/authmanagementservice/config/WebSecurityConfig.kt
@@ -26,6 +26,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 
 @Configuration
 @EnableWebSecurity
@@ -49,6 +50,9 @@ class WebSecurityConfig (
                     .apply(jwtFilterConfigurer)
                     .and()
                     .exceptionHandling().authenticationEntryPoint(authEntryPoint)
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
             if (useSsl) {
                 it.requiresChannel().anyRequest().requiresSecure()
             }
